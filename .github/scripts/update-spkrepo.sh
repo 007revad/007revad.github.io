ca8ccf61c83f6d57d4d19c312db206e98e0e009e
Dave Russell <39733752+007revad@users.noreply.github.com>
2026-09-09 14:36:25 +1000

    Add SynoToolbox to repo

--- .github/scripts/update-spkrepo.sh
+++ .github/scripts/update-spkrepo.sh
@@ -862,7 +862,7 @@ entries+=( "$(make_entries "007revad" "Synology_AQC_Unlock"          "CHANGES.tx
 entries+=( "$(make_entries "007revad" "Syno_Package_Source_Manager"  "CHANGES.txt")" )
 entries+=( "$(make_entries "007revad" "Syno_CPU_Temperature"         "CHANGES.txt")" )
 entries+=( "$(make_entries "007revad" "Synology_iperf3_Speedtest"    "CHANGES.txt")" )
-#entries+=( "$(make_entries "007revad" "Syno_Toolbox"                "CHANGES.txt")" )
+entries+=( "$(make_entries "007revad" "Syno_Toolbox"                 "CHANGES.txt")" )
 
 # Friends' repos
 entries+=( "$(make_entries "PeterSuh-Q3" "SynoSmartInfo"             "")" )
